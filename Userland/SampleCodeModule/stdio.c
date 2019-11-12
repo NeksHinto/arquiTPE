4e2e26de2cc0bf781e8272944b646cd8f9455d7f
Nicolas <nrossi@itba.edu.ar>
2019-11-12 19:51:26 -0300

    Just organizing: IDT related

---
+++ Userland/SampleCodeModule/stdio.c
@@ -0,0 +1,103 @@
+#include <syscall.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void putchar(char c){
+  _syscall(__write_char_buffer, 1, c);
+}
+
+char getchar(){
+	return _syscall(__read_char_buffer, 0);
+}
+
+char getchar_from_buffer(int buffer) {
+  if(buffer <1 || buffer > 2)
+    return 0;
+  return _syscall(__read_char_buffer, buffer);
+}
+
+char putint(uint64_t v, int base) {
+  return _syscall(__write_int_buffer, 1, v, base);
+}
+
+void printf(char* format, ...){
+  va_list arg;
+  va_start(arg, format);
+  int i;
+  char* s;
+  for(; *format != 0; format++){
+    if(*format == '%'){
+      format++;
+      switch(*format){
+            case 'c' :
+              i = va_arg(arg,int);
+              putchar(i);
+              break;
+
+            case 'd' :
+              i = va_arg(arg,int);
+              putint(i, 10);
+             break;
+
+            case 's':
+              s = va_arg(arg,char*);
+              while(*s){
+                putchar(*s);
+                s++;
+              }
+              break;
+      }
+    }else{
+      putchar(*format);
+    }
+  }
+}
+
+int scanf(char* format, ...){
+  va_list arg;
+  va_start(arg, format);
+  int i = 0;
+  char c;
+  int reading = 1;
+  void * loadValue;
+  int valuesLoaded = 0;
+  int number = 0;
+
+  while((c = getchar()) != '\n' && c != 0){
+    if(!reading && c == format[0])
+      reading = 1;
+    if(reading){
+      if(format[i] == '%'){
+        loadValue = va_arg(arg,void *);
+        switch(format[i+1]){
+            case 'c' :
+              *(char *)loadValue = c;
+              break;
+            case 'd' :
+              do{
+                number *= 10;
+                number += (c - '0');
+              }while((c = getchar()) >= '0' && c <= '9');
+
+              *(int *)loadValue = number;
+              number = 0;
+              break;
+            case 's':
+              do{
+                *(char *)loadValue = c;
+                loadValue++;
+              }while((c = getchar()) != ' ' && c != '\n' && c != 0);
+              break;
+        }
+        valuesLoaded++;
+      }
+      else if(c != format[i]){
+        reading = 0;
+        continue;
+      }
+      i++;
+    }
+  }
+  return valuesLoaded;
+}
---
+++ Userland/SampleCodeModule/stdlib.c
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+
+int strcmp(char * s1, char* s2) {
+	int i;
+	for(i = 0; s1[i] != 0 || s2[i] != 0; i++){
+		if(s1[i] != s2[i])
+			return 0;
+	}
+	return 1;
+}
+
+int strlen(char* s) {
+    int i;
+    for(i=0; s[i] != '\0'; i++);
+    return i;
+}
+
+
+char* itoa(int num, int base, char* str) {
+    int i = 0; 
+    int isNegative = 0; 
+  
+    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    if (num == 0) 
+    { 
+        str[i++] = '0'; 
+        str[i] = '\0'; 
+        return str; 
+    } 
+  
+    // In standard itoa(), negative numbers are handled only with  
+    // base 10. Otherwise numbers are considered unsigned. 
+    if (num < 0 && base == 10) 
+    { 
+        isNegative = 1; 
+        num = -num; 
+    } 
+  
+    // Process individual digits 
+    while (num != 0) 
+    { 
+        int rem = num % base; 
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0'; 
+        num = num/base; 
+    } 
+  
+    // If number is negative, append '-' 
+    if (isNegative) 
+        str[i++] = '-'; 
+  
+    str[i] = '\0'; // Append string terminator 
+  
+    // Reverse the string 
+    reverse(str, i); 
+  
+    return str; 
+}
+
+void reverse(char* str, int size) {
+    char aux;
+    for( int i=0; i< size/2; i++) {
+        aux = str[i];
+        str[i] = str[size - i -1];
+        str[size -i -1] = aux;
+    }
+}
+
+int atoi(char *str) { 
+    int res = 0; // Initialize result 
+
+    // Iterate through all characters of input string and 
+    // update result 
+    for (int i = 0; str[i] != '\0'; ++i) 
+        res = res*10 + str[i] - '0'; 
+   
+    // return result. 
+    return res; 
+} 
+
+int isdigit(char c) {
+    return c >= '0' && c <= '9';
+}
