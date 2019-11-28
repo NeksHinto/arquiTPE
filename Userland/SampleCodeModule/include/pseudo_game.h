61ddf3470a18b9a51d6b191e156e99ac5d6bd96a
Nicolas Rossi <nrossi@itba.edu.ar>
2019-11-28 17:28:44 -0300

    time border added

--- Userland/SampleCodeModule/include/pseudo_game.h
+++ Userland/SampleCodeModule/include/pseudo_game.h
@@ -1,10 +1,12 @@
 #ifndef PSEUDO_GAME_H
     #define PSEUDO_GAME_H
 
-#define FALSE 0
-#define MAX_BLOCKS 18
-#define TRUE !FALSE
-#define NULL 0
+#define FALSE           0
+#define MAX_BLOCKS      18
+#define TRUE            !FALSE
+#define NULL            0
+#define BORDER_Y_COORD  50
+#define BORDER_WIDTH    10
 
 typedef struct Position {
     int x,y;
--- Userland/SampleCodeModule/include/video_module.h
+++ Userland/SampleCodeModule/include/video_module.h
@@ -13,6 +13,8 @@ typedef struct Color {
 void write_char(char c, uint64_t x, uint64_t y, Color color );
 //writes a char of the given color and the given size with a starting position of x, y.
 void write_sized_char(char c, uint64_t x, uint64_t y, Color color, Color background, uint64_t size);
+//writes a string of the given color and the given size with a starting position of x, y.
+void write_sized_string(char* s, uint64_t x, uint64_t y, Color color, Color background, uint64_t size, uint64_t spacing);
 //same as write char but with strings.
 void write_string(char* string, uint64_t x, uint64_t y, Color color); 
 //writes a block of pixels of a designated color.
